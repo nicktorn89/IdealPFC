5176fa2866bff8ae2acbe31d640cfa713bb967d6
vlad-komanchy <vkomanchy@gmail.com>
2019-09-04 19:00:03 +0300

    refactor: removing unused packages

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -7,22 +7,21 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 module.exports = merge(commonConfig, {
   mode: 'development',
   entry: [
-    'react-hot-loader/patch', // activate HMR for React
-    // 'webpack-dev-server/client?http://localhost:9090',// bundle the client for webpack-dev-server and connect to the provided endpoint
-    'webpack/hot/only-dev-server', // bundle the client for hot reloading, only- means to only hot reload for successful updates
-    './index.tsx', // the entry point of our app,
+    'react-hot-loader/patch',
+    'webpack/hot/only-dev-server',
+    './index.tsx',
   ],
   devServer: {
-    hot: true, // enable HMR on the server
+    hot: true,
   },
   devtool: 'cheap-module-eval-source-map',
   plugins: [
     new webpack.HotModuleReplacementPlugin(), // enable HMR globally
     new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    // new BundleAnalyzerPlugin({
-    //     openAnalyzer: false,
-    //     analyzerPort: 9995
-    // })
+    new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+        analyzerPort: 9995
+    })
   ],
 });
